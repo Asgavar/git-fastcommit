5ca5aa51dad703e7f19da931bd2f534b85cf6ba7
Artur Juraszek <artur@juraszek.xyz>
2019-01-16 20:45:53 +0100

    Make checkdoc happy

--- magit-fastcommit.el
+++ magit-fastcommit.el
@@ -1,8 +1,19 @@
+;;; magit-fastcommit.el --- Generate commit messages with Magit and two keystrokes
+
+;;; Commentary:
+;; A small package adding a "commit with an auto-generated message"
+;; action to Magit's commit popup.
+
 (require 'magit)
 
+;;; Code:
+
 (defun magit-fastcommit (&optional args)
+  "Generate a UUIDv4 message and commit, passing ARGS down as in normal commit."
   (interactive (list (magit-commit-arguments)))
   (magit-start-git nil "commit" "-m" (uuidgen-4) args))
 
 (magit-define-popup-action 'magit-commit-popup
   ?` "Commit with an auto-generated message" 'magit-fastcommit)
+
+;;; magit-fastcommit.el ends here
