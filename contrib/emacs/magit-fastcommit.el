56359d5863a12ee3b40444e55cf1b0febe03c565
Artur Juraszek <artur@juraszek.xyz>
2019-04-07 19:26:06 +0200

    [emacs] Turn magit-fastcommit into a simple shim invoking git-fastcommit
    No dependencies other than Magit itself needed, yay!

--- contrib/emacs/magit-fastcommit.el
+++ contrib/emacs/magit-fastcommit.el
@@ -1,7 +1,7 @@
 ;;; magit-fastcommit.el --- Generate commit messages with Magit and two keystrokes -*- lexical-binding: t -*-
 
 ;; Author: Artur Juraszek
-;; URL: https://github.com/Asgavar/magit-fastcommit
+;; URL: https://github.com/Asgavar/git-fastcommit
 ;;
 ;;; Commentary:
 ;;
@@ -9,14 +9,13 @@
 ;; action to Magit's commit popup.
 
 (require 'magit)
-(require 'uuidgen)
 
 ;;; Code:
 
 (defun magit-fastcommit (&optional args)
-  "Generate a UUIDv4 message and commit, passing ARGS down as in normal commit."
+  "Automatically generate a message and commit, passing ARGS down as in normal commit."
   (interactive (list (magit-commit-arguments)))
-  (magit-start-git nil "commit" "-m" (uuidgen-4) args))
+  (magit-start-git nil "fastcommit" ))
 
 (magit-define-popup-action 'magit-commit-popup
   ?` "Commit with an auto-generated message" 'magit-fastcommit)
